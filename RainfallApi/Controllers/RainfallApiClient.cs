8d1c190b0357e42ca048d9aa22d202562639d7f8
kevin cris ramos <kramos@nebulex.au>
2024-03-18 16:50:14 +0800

    add one endpoint

--- RainfallApi/Controllers/RainfallApiClient.cs
+++ RainfallApi/Controllers/RainfallApiClient.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
 
 namespace RainfallApi.Controllers
 {
@@ -7,5 +9,40 @@ namespace RainfallApi.Controllers
     [ApiController]
     public class RainfallApiClient : ControllerBase
     {
+        private readonly HttpClient _httpClient;
+        public RainfallApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://environment.data.gov.uk");
+        }
+
+
+        [HttpGet("/flood-monitoring/id/stations/{stationId}")]
+        public async Task<IActionResult> GetStationInfo(int stationId)
+        {
+            try
+            {
+
+                HttpResponseMessage response = await _httpClient.GetAsync($"/flood-monitoring/id/stations/{stationId}");
+
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return Ok(responseBody);
+                }
+                else
+                {
+
+                    return StatusCode((int)response.StatusCode, $"Request failed with status code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle HTTP request error
+                return StatusCode(500, $"HTTP request error: {ex.Message}");
+            }
+        }
     }
 }
--- RainfallApi/Program.cs
+++ RainfallApi/Program.cs
@@ -2,30 +2,25 @@ using RainfallApi.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpClient<RainfallApiClient>(client =>
 {
-    client.BaseAddress = new Uri("http://localhost:3000");
+    client.BaseAddress = new Uri("https://environment.data.gov.uk/");
 });
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rainfall API"));
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
